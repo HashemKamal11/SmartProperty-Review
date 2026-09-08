d083bbc56d6f871ada18cfee291502d8d0ef365b
Hashem Kamal <hashem.kamal@neurix.uk>
2026-09-08 14:39:33 +0300

    Adding controllers architecture

--- src/Presentation/SmartProperty.Api/Program.cs
+++ src/Presentation/SmartProperty.Api/Program.cs
@@ -3,11 +3,18 @@ using SmartProperty.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 builder.Services.AddPersistence(builder.Configuration);
 
 var app = builder.Build();
 
-app.MapHealthChecks("/health/live", new HealthCheckOptions { Predicate = _ => false });
+app.MapControllers();
+
+app.MapHealthChecks("/health/live", new HealthCheckOptions
+{
+    Predicate = _ => false
+});
+
 app.MapHealthChecks("/health/ready", new HealthCheckOptions
 {
     Predicate = registration => registration.Tags.Contains("ready")
